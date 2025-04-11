b6a66f2083bd191a33328d1d0aa23771c1c5c68a
timc1021 <iljfyy@gmail.com>
2025-04-11 16:12:38 +0900

    page 27

--- cra_250411_baseball/baseball.cpp
+++ cra_250411_baseball/baseball.cpp
@@ -1,4 +1,10 @@
+#include "iostream"
+
+using namespace std;
+
 class Baseball {
 public:
-
+	void guess(const string& str) {
+		throw length_error("must be three letters.");
+	}
 };
--- cra_250411_baseball/baseball_test.cpp
+++ cra_250411_baseball/baseball_test.cpp
@@ -1,8 +1,18 @@
 #include <gmock/gmock.h>
 #include "baseball.cpp"
+#include "baseball_test.h"
+#include "iostream"
+#include "stdexcept"
+
+using namespace std;
 
 TEST(BaseballGame, game_test) {
 	EXPECT_EQ(1, 1);
+}
+
+TEST(BaseballGame, ThrowExceptionInputLengthUnmatch) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12")), length_error);
 }
 
 int main() {
